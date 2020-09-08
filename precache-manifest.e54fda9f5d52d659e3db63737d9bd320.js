995a2fadb6adba1da6bded14ea6ecae6d2c44d1a
larry <pascu.laurentiu17@gmail.com>
2020-09-08 11:01:36 +0300

    Updates

---
+++ precache-manifest.e54fda9f5d52d659e3db63737d9bd320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b71fc5fac3da63acc92878eb8f659c",
+    "url": "/loan-calculator/index.html"
+  },
+  {
+    "revision": "c0ed20d3746aa0dd6156",
+    "url": "/loan-calculator/static/css/main.e4c14733.chunk.css"
+  },
+  {
+    "revision": "be3565b67d8c621c3b2c",
+    "url": "/loan-calculator/static/js/2.1e3f3012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loan-calculator/static/js/2.1e3f3012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ed20d3746aa0dd6156",
+    "url": "/loan-calculator/static/js/main.84c3e4ee.chunk.js"
+  },
+  {
+    "revision": "53cea0433bee651416e5",
+    "url": "/loan-calculator/static/js/runtime-main.fd18f442.js"
+  }
+]);
